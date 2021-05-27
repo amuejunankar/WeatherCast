cda74e524b6b7603a225fcaea9c7fd195c32bd7a
amuejunankar <55340245+amuejunankar@users.noreply.github.com>
2021-05-28 00:33:42 +0530

    Still on Working

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
+var inputValue = document.querySelector('.SubmitValue')
 var name = document.querySelector('.name');
 var description = document.querySelector('.description');
 var temp = document.querySelector('.temp');
 
 button.addEventListener('click',function(){
-    fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid={658dcff769c91d18bc57ff4058fbdd7f}')
+    fetch('api.openweathermap.org/data/2.5/weather?q='+SubmitValue.value+'&appid=658dcff769c91d18bc57ff4058fbdd7f')
 
     .then(response => response.json())
     .then(data => console.log(data) )
 
-    .catch (error => alert("Wrong City Name"))
+    .catch (err => alert("Wrong City Name"))
 
 })
